20d1a09260fb4331457a1118b06cb3fc68ba79e5
TonyJev93 <tonyJev93@gmail.com>
2023-05-11 00:16:03 +0900

    init

---
+++ src/main/java/com/tonyjev/resilience/Resilience4jSampleApplication.java
@@ -0,0 +1,13 @@
+package com.tonyjev.resilience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resilience4jSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tonyjev/resilience/Resilience4jSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyjev.resilience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
